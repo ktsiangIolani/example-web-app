a4147706d5e603272aba734161d438ae8e79ed1d
Kimberly Tsiang <ktsiang@KTSIANG.lan>
2023-10-08 20:20:11 -1000

    comment

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
       .catch((error) => console.error(error));
   };
 
-  //------------------------------- COMPONENTS ------------------------------------
+  //------------------------------- COMPO ------------------------------------
 
   const GetUsersButton = () => {
     return (
@@ -73,7 +73,6 @@ function App() {
     );
   };
 
-  //------------------------------- RENDER ------------------------------------
   return (
     <div className="App">
       <div className="App-header">
